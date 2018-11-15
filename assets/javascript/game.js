b996f1f2ed7173045ebb98c6f5b92cd6788d3691
Jenae <Jenaenay15@yahoo.com>
2018-11-15 13:49:49 -0700

    removed obsolete code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,5 @@
-//document.getElementById("rndletter").addEventListener("click", function() {
-//   var result = String.fromCharCode(Math.floor(Math.random() * (122 - 97)) + 97);
-// 
-// document.getElementById("result").innerText = result;
-//}); -->
-//document.getElementById("number").innerHTML="hello world";
-// computer generated choices
 
+// computer generated choices
 var psychicChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //other global variables
@@ -14,57 +8,56 @@ var wins = 1;
 var losses = 1;
 var guessesLeft = 10;
 var lettersGuessed = [];
-var lettersToGuess = null;
+
 
 // random letter selected by the "psychic"
 var psychicGuess = psychicChoice[Math.floor(Math.random() * psychicChoice.length)];
 console.log(psychicGuess);
 
+//Sets the stats for each new round of guesses
+function newGuess() {
+    psychicGuess = psychicChoice[Math.floor(Math.random() * psychicChoice.length)];
+    guessesLeft = 10;
+    lettersGuessed =[];
+};
 //logs the players input and updates the score
 document.onkeydown = function (event) {
-    var verify = psychicGuess.includes(playerChoice);
     var thisGuess = event.key;
+
     playerChoice.push(thisGuess);
 
     if (guessesLeft > 0) {
         if (playerChoice == psychicChoice) {
             wins++;
         }
-        else if(playerChoice !== psychicChoice){
+        else if (playerChoice !== psychicChoice) {
             guessesLeft--;
-            document.getElementById('guessLeft').innerHTML =(guessesLeft);
+            document.getElementById('guessLeft').innerHTML = (guessesLeft);
         }
         console.log(guessesLeft);
 
-        if(playerChoice == psychicGuess) {
-        alert("You really are psychic!!!!");
-        document.getElementById('wins').innerHTML = (wins);
+        if (playerChoice == psychicGuess) {
+            alert("You really are psychic!!!!");
+            document.getElementById('wins').innerHTML = (wins);
             (wins += 1);
-            redo;
-    }
-        else if( guessesLeft === 0){
-        alert("You lose");
-        document.getElementById('losses').innerHTML =(losses);
-        (losses += 1);
-        redo;
+            newGuess();
+        }
+        else if (guessesLeft === 0) {
+            alert("You lose");
+            document.getElementById('losses').innerHTML = (losses);
+            (losses += 1);
+            newGuess();
         }
     }
-    };
-    for (i = 0; i < 10; i++) {
-        document.getElementById('playerGuess').innerHTML = lettersGuessed.join(" , ");
-        console.log(playerChoice);
-    }
-    
     console.log(playerChoice);
     document.getElementById('playerGuess').innerHTML = playerChoice;
-
     lettersGuessed.push(playerChoice);
+    
 
-
-var redo = function () {
-    guessesLeft = 10;
-    lettersGuessed = [];
-}
-    //updateGuessesLeft();
-    //updateLettersGuessed();
+    //allows the guesses to be counted down until it reaches 0
+    //for (i = 0; i < 10; i++) {
+    //   document.getElementById('playerGuess').innerHTML = lettersGuessed.join(" , ");
+    //  console.log(playerChoice);
+    // }
+};
 
